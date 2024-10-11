f9a322e490eb2ec23251d1b1381e14a2a804809e
Alejo Ufano <ufanoalejo@gmail.com>
2024-10-10 21:32:36 -0500

    add: basePath and output to config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	basePath: "/2048-in-react",
+	output: "export", 
 	reactStrictMode: true,
 	webpack: (config) => {
 		config.resolve.extensions.push(".ts", ".tsx");
